0b67838a2ad8fa28d4f678b1d1ba0aa3212affad
yangzhi <hzyangzhi@highstore.cn>
2020-04-03 15:11:12 +0800

    初始化

---
+++ src/main/java/com/example/fileuddemo/FileuddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileuddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuddemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileuddemoApplication.class, args);
+    }
+
+}
